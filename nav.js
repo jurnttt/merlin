3edcf78969fcaf97a24aabb459526b31dc263c54
jurnttt <jurntt@gmail.com>
2026-07-15 15:14:31 +0100

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,20 +1,20 @@
 document.write('<nav class="navbar navbar-inverse">\
 <div class="container-fluid">\
   <ul class="nav navbar-nav">\
-    <li class="active"><a href="https://dev-1113.github.io/merlin/index.html">Home</a></li>\
+    <li class="active"><a href="https://jurnttt.github.io/merlin/index.html">Home</a></li>\
     <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">Front-End Mentor<span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="https://dev-1113.github.io/merlin/product/index.html">Product Preview</a></li>\
-        <li><a href="https://dev-1113.github.io/merlin/interactive-rating-component-main/index.html">Interactive Rating</a></li>\
-        <li><a href="https://dev-1113.github.io/merlin/qr-code-component-main/index.html">QR Component</a></li>\
-        <li><a href="https://dev-1113.github.io/merlin/nft-preview-card-component-main/index.html">Preview Card</a></li>\
-        <li><a href="https://dev-1113.github.io/merlin/order-summary-component-main/index.html">Order Summary</a></li>\
-        <li><a href="https://dev-1113.github.io/merlin/3-column-preview-card-component-main/index.html">3 Column Card</a></li>\
+        <li><a href="https://jurnttt.github.io/merlin/product/index.html">Product Preview</a></li>\
+        <li><a href="https://jurnttt.github.io/merlin/interactive-rating-component-main/index.html">Interactive Rating</a></li>\
+        <li><a href="https://jurnttt.github.io/merlin/qr-code-component-main/index.html">QR Component</a></li>\
+        <li><a href="https://jurnttt.github.io/merlin/nft-preview-card-component-main/index.html">Preview Card</a></li>\
+        <li><a href="https://jurnttt.github.io/merlin/order-summary-component-main/index.html">Order Summary</a></li>\
+        <li><a href="https://jurnttt.github.io/merlin/3-column-preview-card-component-main/index.html">3 Column Card</a></li>\
       </ul>\
     </li>\
-    <li><a href="https://dev-1113.github.io/merlin/project.html">UNI Project</a></li>\
-    <li><a href="https://dev-1113.github.io/merlin/taxishare.html">Taxi Sharing App</a></li>\
-    <li><a href="https://dev-1113.github.io/merlin/modal-gallery/gallery.html">Modal Gallery</a></li>\
+    <li><a href="https://jurnttt.github.io/merlin/project.html">UNI Project</a></li>\
+    <li><a href="https://jurnttt.github.io/merlin/taxishare.html">Taxi Sharing App</a></li>\
+    <li><a href="https://jurnttt.github.io/merlin/modal-gallery/gallery.html">Modal Gallery</a></li>\
   </ul>\
 </div>\
 </nav>\
